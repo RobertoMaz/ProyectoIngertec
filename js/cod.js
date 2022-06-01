325d4c2272e0a92affaf4d03a6c0612e976534e8
RobertoMaz <100147627+RobertoMaz@users.noreply.github.com>
2022-06-01 17:30:33 -0300

    Update cod.js
    w

--- js/cod.js
+++ js/cod.js
@@ -20,6 +20,7 @@ import cargarTexto from "./cargarTexto.js";
 import contadorVisitas from "./contadorVisitas.js";
 
 
+
 contadorVisitas(localStorage.getItem("contadorImg1", "value"),
                 localStorage.getItem("contadorImg2", "value"), 
                 localStorage.getItem("contadorImg3", "value"),
@@ -27,22 +28,33 @@ contadorVisitas(localStorage.getItem("contadorImg1", "value"),
                 localStorage.getItem("contadorImg5", "value"));
 
 cargarTexto();
-/*
+
+
+
+
 let objWindowOpen;
 const configVentana = "menubar=no,location=no,resizable=yes,scrollbars=no,status=no,width=800,height=400";
 
-function abrirPopup(nombreImagen, img) {
-    objWindowOpen = window.open(encodeURI(`http://127.0.0.1:5500/${nombreImagen}`), img, configVentana);
-}
 
 const
-//aImg1 = document.getElementById("aimg1"),
-aImg3 = document.getElementById("aimg3"),
-aImg5 = document.getElementById("aimg5");
+    aImg1 = document.getElementById("aimg1"),
+    aImg3 = document.getElementById("aimg3"),
+    aImg5 = document.getElementById("aimg5");
+      
+
+function abrirPopup(nombreImagen, img) {
+  
+  objWindowOpen = window.open(encodeURI(`http://127.0.0.1:5500/${nombreImagen}`), img, configVentana);
+    
+    
+    
+}
+
 
 
 aImg1.addEventListener("click", () => {
   abrirPopup("imagen1html.html", "Imagen1");
+  
 });
 
 aImg3.addEventListener("click", () => {
@@ -52,29 +64,15 @@ aImg3.addEventListener("click", () => {
 aImg5.addEventListener("click", () => {
   abrirPopup("imagen5html.html", "Imagen5");
 });
-*/
-
-
-const btnOkImg1 = document.getElementById("btn-okimg1"),
-      btnOkImg3 = document.getElementById("btn-okimg3"),
-      btnOkImg5 = document.getElementById("btn-okimg5");
-
-let vimg1 = localStorage.getItem("contadorImg1", "value"),
-    vimg3 = localStorage.getItem("contadorImg3", "value"),
-    vimg5 = localStorage.getItem("contadorImg5", "value");
-
-const parrafoImagen1 = document.getElementById("pimg1"),
-      parrafoImagen3 = document.getElementById("pimg3"),
-      parrafoImagen5 = document.getElementById("pimg5");
 
 
 
-      btnOkImg1.addEventListener("click", () => {
-        vimg1++;
-        localStorage.setItem("contadorImg1", vimg1);
-        parrafoImagen1.innerHTML = vimg1;
-    });
 
+
+
+
+      
+/*
     btnOkImg3.addEventListener("click", () => {
       vimg3++;
         localStorage.setItem("contadorImg3", vimg3);
@@ -85,4 +83,4 @@ const parrafoImagen1 = document.getElementById("pimg1"),
         vimg5++;
         localStorage.setItem("contadorImg5", vimg5);
         parrafoImagen5.innerHTML = vimg5;
-    });
+    });*/
