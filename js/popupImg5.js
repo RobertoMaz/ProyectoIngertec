1ad9d62f836110c13e0d506f43a6c262e8b500c5
RobertoMaz <100147627+RobertoMaz@users.noreply.github.com>
2022-06-02 23:20:43 -0300

    Update popupImg5.js

--- js/popupImg5.js
+++ js/popupImg5.js
@@ -1,9 +1,7 @@
-const btnOkImg5 = document.getElementById("btn-okimg5");
+const btnOkImg5 = document.getElementById("btn-okimg5"); // Capturamos el boton ok, del popup.
+let vimg5 = localStorage.getItem("contadorImg5", "value"); // vamos a buscar los datos guardados en "localStorage".
 
-let vimg5 = localStorage.getItem("contadorImg5", "value");
-
-
-
+// Evento del boton para submar vistas y cerrar la ventana.
 btnOkImg5.addEventListener("click", () => {
     vimg5++;
     localStorage.setItem("contadorImg5", vimg5);
