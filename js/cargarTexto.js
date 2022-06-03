48271ebb40316c65bcaea2eb702a159633c12926
RobertoMaz <100147627+RobertoMaz@users.noreply.github.com>
2022-06-02 23:18:07 -0300

    Update cargarTexto.js

--- js/cargarTexto.js
+++ js/cargarTexto.js
@@ -1,19 +1,21 @@
-const btnCargarTexto = document.getElementById("btn-cargartexto"),
-      texto = document.getElementById("text"),
-      divBtn = document.getElementById("div-btn");
+const btnCargarTexto = document.getElementById("btn-cargartexto"), // Captura el boton "Cargar Texto".
+      texto = document.getElementById("text");  // Captura el area donde vamosa insetar el texto.
 
 export default function cargarTexto(){
     btnCargarTexto.addEventListener("click", () => {
-        fetch("home.json")
+        fetch("home.json") // Peticion JSON.
             .then((res) => (
                 res.ok 
                     ? res.json() 
                     : Promise.reject(res)))
             .then((json) => 
-                texto.value = json.texto)
+                texto.value = json.texto) // Si todo sale bien. imprime el JSON en area del texto.
             .catch((err) =>{
+                // Por lo contrario si hay un error, tira el dato del error si no lo encuentra imprime "Ocurrio un error."
+                btnCargarTexto.classList.remove("btn-success"); 
+                btnCargarTexto.classList.add("btn-danger");
                 let mensaje = err.statusText || "Ocurrio un error, no se encontro el texto.";
-                divBtn.innerHTML = `<p>Error ${err.status}: ${mensaje}</p>`
+                btnCargarTexto.innerHTML = `<p>Error ${err.status}: ${mensaje}</p>`
             });
     })
 }
